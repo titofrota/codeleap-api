34a08f8e17f659a67e3e617236eaf43ee30ae394
titofrota <italoeduardo.df@hotmail.com>
2024-12-15 00:47:35 -0300

    :building_construction: refactor: remove services

--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from api.models import Post
 from api.serializers import PostSerializer
-from api.services import post_service
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
