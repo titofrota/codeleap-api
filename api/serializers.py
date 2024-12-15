c5ed1baf2f1196483b466f4f9b8426f6460fbf78
titofrota <italoeduardo.df@hotmail.com>
2024-12-15 01:15:52 -0300

    :construction: wip: username validation

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,9 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
-        read_only_fields = ['id', 'username', 'created_datetime']
+        read_only_fields = ['id', 'created_datetime']
+
+    def validate(self, attrs):
+        if self.instance and 'username' in attrs:
+            raise serializers.ValidationError('username field cannot be updated')
+        return attrs
