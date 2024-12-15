ee0eebcdacf5b3c61ca0c19f96f256566fb93695
titofrota <italoeduardo.df@hotmail.com>
2024-12-14 23:47:17 -0300

    :sparkles: feat: add post admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api.models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('username', 'title', 'created_datetime')
+    search_fields = ('username', 'title')
+    list_filter = ('created_datetime',)
+
+admin.site.register(Post, PostAdmin)
